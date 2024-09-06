1b966a71a67edea166d73dc5a86c209b6fdb4502
rescalon34 <roberto.escalon34@gmail.com>
2024-09-06 16:59:26 -0600

    🚚 develop - setup iOS theme and package organization for the presentation layer.

---
+++ iosApp/iosApp/Core/Theme/CustomColors.swift
@@ -0,0 +1,23 @@
+//
+//  CustomColors.swift
+//  iosApp
+//
+//  Created by rescalon on 6/9/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+/// Map each custom color. It supports dark/ligth color themes.
+struct CustomColors {
+    
+    let accentColor = Color("AccentColor")
+    let backgroundColor = Color("BackgroundColor")
+    let secondaryBackgroundColor = Color("SecondaryBackgroundColor")
+    let primaryTextColor = Color("PrimaryTextColor")
+    let primaryClearTextColor = Color("PrimaryClearTextColor")
+    let secondaryTextColor = Color("SecondaryTextColor")
+    let categoryCapsuleColor = Color("CategoryCapsuleColor")
+    let categoryCapsuleLigthColor = Color("CategoryCapsuleLigthColor")
+    let dismissViewIconColor = Color("DismissViewIconColor")
+}
