31d82b766356e0a71c170257318b1c2695dddf35
rescalon34 <roberto.escalon34@gmail.com>
2024-09-06 16:39:53 -0600

    🚚 develop - setup Android theme and package organization for the presentation layer

---
+++ androidApp/src/main/java/com/escalondev/movies_app_kmp/android/ui/home/HomeScreen.kt
@@ -0,0 +1,36 @@
+package com.escalondev.movies_app_kmp.android.ui.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.escalondev.movies_app_kmp.android.theme.MoviesAppTheme
+
+@Composable
+fun HomeScreen() {
+    HomeContent()
+}
+
+@Composable
+fun HomeContent() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home Screen", style = MaterialTheme.typography.titleLarge)
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun HomeContentPreview() {
+    MoviesAppTheme {
+        HomeContent()
+    }
+}
