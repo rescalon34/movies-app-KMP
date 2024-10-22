6d51f64ffc16af133179e4452a9d439c697b90d9
rescalon34 <roberto.escalon34@gmail.com>
2024-10-21 21:06:03 -0600

    🔥MOV-KMP-refactor-initialization - Remove dead code.

--- shared/src/androidMain/kotlin/com/escalondev/movies_app_kmp/core/util/KoinUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.escalondev.movies_app_kmp.core.util
-
-import org.koin.core.context.GlobalContext
-
-/**
- * Extension function to have a handy access to the Koin `GlobalContext`
- * from the consumer side when setting up the SharedCoreManager instance.
- */
-fun getKoinGlobalContext() = GlobalContext.get()
